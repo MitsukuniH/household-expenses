92fb06ed60759a4859fd0b0e37af9f6d65239e3e
MitsukuniH <hmitsuku3292@gmail.com>
2022-10-26 10:37:19 +0900

    create frame

---
+++ client/src/BODY.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const BODY = () => {
+  return (
+    <body>
+      <header>
+				<h1>My Expenses</h1>
+      </header>
+			<main>
+			</main>
+    </body>
+  )
+}
+
+export default BODY
---
+++ client/src/HEAD.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const HEAD = () => {
+  return (
+    <head>
+        <title>My Expenses</title>
+        <meta charSet='UTF-8'/>
+    </head>
+  )
+}
+
+export default HEAD
